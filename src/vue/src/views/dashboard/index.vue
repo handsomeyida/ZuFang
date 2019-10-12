aabe98ec7385be275ddd2c1998b44701b555fa83
帅气的达 <867256260@qq.com>
2019-10-12 09:53:53 +0800

    update main picture

--- src/vue/src/views/dashboard/index.vue
+++ src/vue/src/views/dashboard/index.vue
@@ -2,7 +2,7 @@
   <div class="dashboard-container">
     <div class="tx">
       <a href="https://user.qzone.qq.com/867256260/infocenter" target="_blank"><img class="tximg"
-                                                                                    src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565870854203&di=2715c7960d3705a89539551832c221ca&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F17%2F03%2F03%2F8721263b7aff2e254e92ef07e53304a6.jpg%2521%2Ffwfh%2F804x485%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue">
+                                                                                    src="http://pic85.nipic.com/file/20151218/17926741_084024242000_2.jpg">
       </a>
     </div>
   </div>
